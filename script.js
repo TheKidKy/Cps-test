b57f3bc4eec89b25ca04e5b6fd2300a46e354060
TheKidKy <imsajadky@gmail.com>
2022-11-17 23:47:28 -0800

    updated ui

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var sec = document.getElementById('sec');
 var btn = document.getElementById('btnClick');
+var result = document.getElementById('result')
 var click_count;
 var time;
 
 function Click()
 {
-   
     click_count = document.getElementById('click').innerHTML++;
     time = setTimeout(Timer, 10000);
-
 }
 
 function Timer()
 {
     btn['disabled'] = true;
     clearTimeout(time);
-    document.getElementById('result').innerHTML = click_count / 10.00;
+    var cps = click_count / 10.00;
+    result.innerHTML = cps;
+
+    if (cps <= 3) {
+        result.style.color = 'red';
+    }
+    else if (cps <= 5) {
+        result.style.color = 'green';
+    }
+    else {
+        result.style.color = 'gold';
+    }
 }
 
 function Reset() {
